3708b1366d86249eb580ecea07a24281a3d16274
dengzq <dzq@163.com>
2017-06-30 22:22:33 +0800

    第一次提交博学谷项目,完成了讲师列表,注销功能和讲师信息查看

---
+++ js/teacher/list.js
@@ -0,0 +1,50 @@
+//讲师列表
+define(["jquery", "text!tpls/teacherList.html", "arttemplate","teacher/teacherMt"], function ($, teacherListTpl, art,teacherMt) {
+  // console.log(teacherListTpl)导入的字符串;
+  //art art接受了arttemplate的返回值;
+  return function () {
+    //3个参数: url  data    success
+    $.get("/api/teacher", {}, function (res) {
+      console.log(res);
+      //数据
+      //  if(res.code ==200){
+      //    //数据加载表格中;
+
+      //  }else{
+      //    //异常
+      //    throw new Error(res.msg);
+      //  }
+      if (res.code !== 200) throw new Error(res.msg);
+      //到这里说明数据返回了.优化后少了if语句;
+      var teacherList = art.render(teacherListTpl, res);
+      //将字符串转换为dom,把该dom放到body中之前给该对象注册事件,$(字符串)把该字符串变成一个jquery对象,用变量接收它,因为每写一次$(字符串)都创造了新的对象;
+      var newteacher = $(teacherList);
+      newteacher.on("click", ".btn-status", function () {
+        //  alert("修改用户状态");
+        var $btn = $(this);
+        var data = {
+          tc_id: $(this).parent().attr("tc_id"),
+          tc_status: $(this).parent().attr("tc_status")
+        }
+        $.post("api/teacher/handle", data, function (res) {
+          if (res.code != 200) throw new Error(res.msg);
+          //获取更新后的状态值;
+          var tc_status = res.result.tc_status;
+          //1修改页面文本
+          $btn.text(tc_status == 0 ? "注销" : "启用");
+          //2.修改传入的数据,
+          $btn.parent().attr("tc_status", tc_status);
+          //3.修改状态页面文本
+          $btn.parent().siblings(".td-status").text(tc_status == 0 ? "启用" : "禁用")
+        });
+
+      }).on("click",".btn-primary",function (){
+          var tc_id= $(this).parent().attr("tc_id");
+           teacherMt(tc_id);
+      })
+      //传入字符串,通过模板引擎的方法将对象信息填上去;
+      $(".menu-content-container").empty().append(newteacher);
+      //清空内容,再追加.不能用html的原因是:html(字符串)是创建了新的对象替换到内容里去.此对象就不是原来绑定了事件的那个对象了.
+    });
+  }
+})
---
+++ main.js
@@ -0,0 +1,49 @@
+//main require导入页;
+require.config({
+    baseUrl: "js",
+    paths: {
+        jquery: "lib/jquery-2.1.4",
+        cookie: "lib/jquery.cookie",
+        bootstrap: "../assets/bootstrap/js/bootstrap",
+        list:"teacher/list",
+        text:"lib/text",
+        tpls:"../tpls",
+        arttemplate:"lib/template-web"
+    },
+    shim: {
+        bootstrap: {
+            deps: ["jquery"]
+        }
+    }
+})
+
+require(["jquery","list", "cookie", "bootstrap"], function ($,teacherlist) {
+    var userInfoStr = $.cookie("userInfo");
+    // console.log(userInfoStr);
+    if (!userInfoStr) {
+        location.href = "login.html";
+    }
+    var userInfo = JSON.parse(userInfoStr);
+    console.log(userInfo);
+    console.log(userInfo.tc_avatar);
+    //2.更新页面头像
+    $(".profile-container .img-circle").attr("src", userInfo.tc_avatar);
+    $(".profile-container .text-center").text(userInfo.tc_name);
+    //3.实现菜单栏的切换
+    $(".aside .list-group").on("click","button", function () {
+                    //通过事件委托,给每个按钮注册点击事件;
+          $(this).addClass("active").siblings().removeClass("active");
+        if ($(this).hasClass("btn-teacher")) {
+            teacherlist();
+        } else if ($(this).hasClass("btn-course")) {
+            $(".menu-content-container").html("<h1 style='margin:0'>课程管理</h1>")
+        } else if ($(this).hasClass("btn-cours-category")) {
+            $(".menu-content-container").html("<h1 style='margin:0'>课程分类</h1>")
+        } else if ($(this).hasClass("btn-chart")) {
+            $(".menu-content-container").html("<h1 style='margin:0'>图表统计</h1>")
+        }
+    });
+    //4.触发讲师管理的点击事件,属性页面默认来点击;
+    $(".aside .list-group button.btn-teacher").trigger("click");
+
+})
